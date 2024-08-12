cef94810b4d880ea7f0a9d949426402b5e1d8bef
Delirium/Arkoudokinigos <panoschuvardas@gmail.com>
2024-08-12 22:25:38 +0300

    Add files via upload

--- mods/unitframes/deli_unitframes.lua
+++ mods/unitframes/deli_unitframes.lua
@@ -22,6 +22,111 @@ end
 
 module.enable = function(self)
 
+------------------------RESTED------------------------
+
+
+-----Reskins the zzz-----
+-- Create a new frame
+--local overlay = CreateFrame("Frame", nil, PlayerRestIcon:GetParent())
+local overlay = CreateFrame("Frame", "overlay", UIParent)
+overlay:SetWidth(24)
+overlay:SetHeight(24)
+overlay:SetPoint("CENTER", PlayerFrame, "CENTER", -20, 30) -- Position the NEW RESTED icon
+
+--remove the original blizzard zzz
+PlayerRestIcon:SetTexture("")
+PlayerRestIcon:ClearAllPoints()
+PlayerRestIcon:SetPoint("TOPLEFT", PlayerFrame, -3000, 0)
+
+-- Set the frame strata to be higher than PlayerRestIcon
+--overlay:SetFrameStrata("DIALOG")
+
+-- Set the size and position of the overlay to match PlayerRestIcon
+--overlay:SetWidth(PlayerRestIcon:GetWidth())
+--overlay:SetHeight(PlayerRestIcon:GetHeight())
+--overlay:SetPoint("CENTER", PlayerRestIcon, "CENTER")
+
+-- Set the texture for the overlay
+overlay.texture = overlay:CreateTexture()
+overlay.texture:SetAllPoints()
+overlay.texture:SetTexture("Interface\\AddOns\\tDF\\img\\Unitframe\\UIUnitFrameRestingFlipbook.tga")
+--overlay.texture:SetTexture("Interface\\AddOns\\tDF\\img\\Unitframe\\flipbookrested-animation.tga")
+overlay.texture:SetTexCoord(0/512, 60/512, 0/512, 60/512)
+-----Reskins the zzz-----
+
+----------------ANIMATION----------------
+-- Define the TexCoords for each frame of the animation
+local texCoords = {
+  --x changes by 60 y changes by 60
+  -- (1)512 (x)    x    512 (y)      (2)512 (x)    x    512 (y)      (3)512 (x)    x    512 (y)       (4)512 (x)    x    512 (y)       (5)512 (x)    x    512 (y)       (6)512 (x)    x    512 (y)
+  {0/512, 60/512, 0/512, 60/512}, {60/512, 120/512, 0/512, 60/512}, {120/512, 180/512, 0/512, 60/512}, {180/512, 240/512, 0/512, 60/512}, {240/512, 300/512, 0/512, 60/512}, {300/512, 360/512, 0/512, 60/512}, --z
+  {0/512, 60/512, 60/512,120/512}, {60/512, 120/512, 60/512, 120/512}, {120/512, 180/512, 60/512, 120/512}, {180/512, 240/512, 60/512, 120/512}, {240/512, 300/512, 60/512, 120/512}, {300/512, 360/512, 60/512, 120/512}, --zz
+  {0/512, 60/512, 120/512, 180/512}, {60/512, 120/512, 120/512, 180/512}, {120/512, 180/512, 120/512, 180/512}, {180/512, 240/512, 120/512, 180/512}, {240/512, 300/512, 120/512, 180/512}, {300/512, 360/512, 120/512, 180/512}, --zz
+  {0/512, 60/512, 180/512, 240/512}, {60/512, 120/512, 180/512, 240/512}, {120/512, 180/512, 180/512, 240/512}, {180/512, 240/512, 180/512, 240/512}, {240/512, 300/512, 180/512, 240/512}, {300/512, 360/512, 180/512, 240/512}, --zzz
+  {0/512, 60/512, 240/512, 300/512}, {60/512, 120/512, 240/512, 300/512}, {120/512, 180/512, 240/512, 300/512}, {180/512, 240/512, 240/512, 300/512}, {240/512, 300/512, 240/512, 300/512}, {300/512, 360/512, 240/512, 300/512}, --zzz
+  {0/512, 60/512, 300/512, 360/512}, {60/512, 120/512, 300/512, 360/512}, {120/512, 180/512, 300/512, 360/512}, {180/512, 240/512, 300/512, 360/512}, {240/512, 300/512, 300/512, 360/512}, {300/512, 360/512, 300/512, 360/512}, --zzz
+  --{0/512, 60/512, 360/512, 410/512}, {60/512, 120/512, 360/512, 410/512}, {120/512, 180/512, 360/512, 410/512}, {180/512, 240/512, 360/512, 410/512}, {240/512, 300/512, 360/512, 410/512}, {300/512, 360/512, 360/512, 410/512}, --zzz
+
+}
+--[[
+-- Define the TexCoords for each frame of the animation
+local texCoords = {
+  -- 512 is the x, 64 is the y
+  {100/512, 120/512, 34/64, 57/64}, --z
+  {163/512, 200/512, 19/64, 57/64}, --zz
+  {227/512, 280/512, 4/64, 57/64}, --zzz
+  {302/512, 344/512, 3/64, 46/64}, --zz
+  {379/512, 408/512, 3/64, 36/64}, --z
+
+}
+]]
+
+-- Function to update the texture coordinates
+local currentFrame = 1
+local function UpdateTexCoords()
+  local coords = texCoords[currentFrame]
+  overlay.texture:SetTexCoord(unpack(coords))
+  currentFrame = currentFrame + 1
+  if currentFrame > table.getn(texCoords) then
+      currentFrame = 1
+  end
+end
+
+-- OnUpdate script to change the TexCoords every 0.1 seconds
+local timeSinceLastUpdate = 0
+local updateInterval = .05 -- .1 default - Adjust this to change the speed of the animation
+overlay:SetScript("OnUpdate", function(self, elapsed)
+  local elapsed = arg1 or 0
+  timeSinceLastUpdate = timeSinceLastUpdate + elapsed
+  if timeSinceLastUpdate > updateInterval then
+      timeSinceLastUpdate = 0
+      UpdateTexCoords()
+  end
+end)
+----------------ANIMATION----------------
+
+----------------SHOW/HIDE ANIMATION----------------
+overlay:Hide()
+
+-- Function to check and update the resting state
+local function UpdateRestingState()
+    if IsResting() then
+        overlay:Show()
+    else
+        overlay:Hide()
+    end
+end
+
+-- Register the event
+overlay:RegisterEvent("PLAYER_UPDATE_RESTING")
+overlay:RegisterEvent("PLAYER_ENTERING_WORLD") -- Add this event to check on login
+overlay:SetScript("OnEvent", UpdateRestingState)
+
+-- Initial check when the addon is loaded
+UpdateRestingState()
+
+----------------SHOW/HIDE ANIMATION----------------
+
 	
 --hide
 PlayerFrame.name:Hide()
